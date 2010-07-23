0abef95ceb9c1f9c8ec2b26e9322cafcbb23da28
Gregory Brown <gregory.t.brown@gmail.com>
2010-07-23 12:15:42 -0400

    Mendibot's first breath

---
+++ mendibot.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'isaac'
+require 'bot_config'
+
+on :connect do
+  join "#rmu-general rmu1337"
+end
+
+on :channel, /^!site$/ do
+  msg channel, "http://university.rubymendicant.com"
+end
+
+on :channel, /^!topic (.*)/ do
+  $topic = match[0]
+  msg channel, "The topic is now #$topic"
+end
+
+on :channel, /^!topic$/ do
+  msg channel, "The topic is currently #$topic"
+end
+
+on :channel do
+  msg channel, [nick, message, user, host, Time.now.to_s].inspect
+end
+
+on :private, /^site$/ do
+  msg nick, "http://university.rubymendicant.com"
+end
