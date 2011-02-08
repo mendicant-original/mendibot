66701c17b18d669aa558a25d75c78709feedafac
Shane Emmons <semmons99@gmail.com>
2011-02-07 19:16:08 -0500

    update to ipinfodb api

--- lib/ip2geo.rb
+++ lib/ip2geo.rb
@@ -3,7 +3,7 @@ require 'rexml/document'
 
 module IP2GEO
   def self.retreive(ip)
-    xml = RestClient.get("ipinfodb.com/ip_query.php?ip=#{ip}&timezone=false")
+    xml = RestClient.get("api.ipinfodb.com/v2/ip_query.php?key=e91ab06882367de54fd7e394c04f3292d3d50761c78e8691c03909ddad2e8391&ip=#{ip}&timezone=false")
     doc = REXML::Document.new(xml)
     lat = doc.elements.to_a('/Response/Latitude').first.text
     lng = doc.elements.to_a('/Response/Longitude').first.text
