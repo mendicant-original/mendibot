c29c8c4d63002c22afd883373249e51d7cd60e20
Jordan Byron <jordan@ducksoupsoftware.com>
2011-02-27 20:20:12 -0500

    Update timezone command

--- plugins/timezone.rb
+++ plugins/timezone.rb
@@ -9,10 +9,10 @@ module Mendibot
       include Cinch::Plugin
 
       match /time help/,              method: :help
-      match /time (.*) (.*) in (.*)/, method: :convert
+      match /time (.*) (.*) to (.*)/, method: :convert
 
       def help(m)
-        m.reply "#{m.user.nick}: <!time (time) (from) in (to)> converts the time from one timezone to another"
+        m.reply "#{m.user.nick}: <!time (time) (from) to (to)> converts the time from one timezone to another"
       end
 
       def convert(m, time, from, to)
