10e7a2b563d21cf1eef9502607d1493669bf5bb0
Jordan Byron <jordan@ducksoupsoftware.com>
2011-12-23 17:45:01 -0500

    Add capistrano to project

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ source :rubygems
 gem 'cinch'
 gem 'rest-client'
 gem 'daemons'
+
+gem 'capistrano'
---
+++ config/deploy.rb
@@ -0,0 +1,29 @@
+require 'bundler/capistrano'
+
+set :application, "mendibot"
+set :repository,  "git://github.com/mendicant-university/mendibot.git"
+
+set :scm, :git
+set :deploy_to, "/var/rapp/#{application}"
+
+set :user, "git"
+set :use_sudo, false
+
+set :deploy_via, :remote_cache
+
+set :branch, "master"
+server "mendicantuniversity.org", :app, :web, :db, :primary => true
+
+before 'deploy:update_code' do
+  run "sudo god stop mendibot"
+end
+
+after 'deploy:update_code' do
+  run "ln -nfs #{shared_path}/environment.rb #{release_path}/config/environment.rb"
+end
+
+after 'deploy' do
+  run "sudo god start mendibot"
+end
+
+after "deploy", 'deploy:cleanup'
