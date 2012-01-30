b173c582be900920d8932f04dcef00c52a4b8137
Brian Glusman <brian@enjyn.com>
2012-01-30 12:39:13 -0500

    Notice correct spelling

--- plugins/spell.rb
+++ plugins/spell.rb
@@ -17,8 +17,15 @@ module Mendibot
       end
 
       def suggest(m, word)
-        suggestions = @hunspell.suggest(word).join(', ')
-        m.reply "#{m.user.nick}: #{word} -> #{suggestions}"
+        suggestions = @hunspell.suggest(word)
+        if suggestions.first != word
+          suggestions = suggestions.join(', ')
+          m.reply "#{m.user.nick}: #{word} -> #{suggestions}"
+        else
+          suggestions = suggestions[1..-1].join(', ')
+          m.reply "#{m.user.nick}:Looks like you spelled it right!  
+                    Some other ideas for you just in case: #{suggestions}"
+        end
       rescue Exception => e
         m.reply "Failed to spell #{word}"
         bot.logger.debug e.message
