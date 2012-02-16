9477cae971129fc372ddfdb059aff69189252387
Shane Emmons <semmons99@gmail.com>
2012-02-16 14:13:41 -0500

    first stab at generalized rss. in reference to issue #30

---
+++ plugins/rss.rb
@@ -0,0 +1,34 @@
+require 'cinch'
+require 'open-uri'
+require 'rss'
+
+module Mendibot
+  module Plugins
+    class RSS
+      include Cinch::Plugin
+
+      timer Mendibot::Config::RSS[:interval], method: :pull
+
+      def pull
+        Mendibot::Config::RSS[:feeds].each do |feed|
+          open(feed[:url]) do |rss|
+            feed     = RSS::Parser.parse(rss)
+            min_time = Time.now - Mendibot::Config::RSS[:interval]
+
+            feed.items.each do |item|
+              if item.pubDate >= min_time
+                feed[:channels].each do |chan|
+                  Channel(chan).send <<-MESSAGE
+via #{feed[:name]} comes the epic saga "#{item.link}", by #{item.author}!
+MESSAGE
+                end
+              end
+            end
+          end
+        end
+      rescue Exception => e
+        bot.logger.debug e.message
+      end
+    end
+  end
+end
