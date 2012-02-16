e472370176456c092c6c33acd4ccf070ced9c111
Shane Emmons <semmons99@gmail.com>
2012-02-16 15:46:37 -0500

    cleanup some bad naming and drop author since it's not reliable

--- plugins/rss_relay.rb
+++ plugins/rss_relay.rb
@@ -12,14 +12,16 @@ module Mendibot
       def pull
         Mendibot::Config::RSS_SETTINGS[:feeds].each do |feed|
           open(feed[:url]) do |rss|
-            feed     = ::RSS::Parser.parse(rss)
+            data     = ::RSS::Parser.parse(rss)
             min_time = Time.now - Mendibot::Config::RSS_SETTINGS[:interval]
 
-            feed.items.each do |item|
+            data.items.each do |item|
+              next unless item.respond_to?(:pubDate)
+
               if item.pubDate >= min_time
                 feed[:channels].each do |chan|
                   Channel(chan).send <<-MESSAGE
-via #{feed[:name]} comes the epic saga "#{item.link}", by #{item.author}!
+via #{feed[:name]} comes the epic saga "#{item.title}" (#{item.link})!
 MESSAGE
                 end
               end
